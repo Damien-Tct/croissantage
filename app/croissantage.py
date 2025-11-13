c341c96af5154e97fa9b7bf830fe5ed9e3e277ff
Damien <damien.tranchant@tctfamily.fr>
2025-11-13 21:58:21 +0100

    retrait biblio non nécessaires

--- app/croissantage.py
+++ app/croissantage.py
@@ -1,9 +1,6 @@
 #! /usr/bin/env python3
 
 from flask import Flask, render_template, request
-from datetime import datetime, timedelta
-
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
